dc469ba6b4c89ce0389f54a4ddd1d5db399f5dbd
SebastianBessoudo <sbessoud@calpoly.edu>
2024-01-30 13:27:50 -0800

    put in timer for output of LED
    hello

--- src/main.py
+++ src/main.py
@@ -1,2 +1,8 @@
+import pyb
+improt Timer
+
 # sets the output of the pin to PA_0
 pinA0 = pyb.Pin(pyb.Pin.board.PC0, pyb.Pin.OUT_PP)
+tim2 = pyb.Timer(2, freq=20000)
+ch2 = tim2.channel(2, pyb.Timer.PWM, pin=pinA1)
+ch2.pulse_width_percent(30)
