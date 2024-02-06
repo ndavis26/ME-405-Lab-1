da2b4b69fe9b3842a3b3894667a1c14fce540f9c
ndavis26 <158105326+ndavis26@users.noreply.github.com>
2024-02-05 20:25:11 -0800

    Coding Done
    Final Iteration on Coding Done. Doxygen still needed.

---
+++ src/led.py
@@ -0,0 +1,46 @@
+"""! @file led.py
+Slowly increases and then decreases LED brightness over 10 seconds.
+Requires LED with anode connected to 3v3 pin on Arduino and cathode connected to pin A0 on Arduino.
+Place a resistor in circuit as well.
+"""
+
+import pyb
+import time
+
+def led_setup():
+    """"!
+    Sets up LED configuration by definind output pin and timer to be used for PWM.
+    """
+    global A0
+    pinA0 = pyb.Pin(pyb.Pin.board.PA0, pyb.Pin.OUT_PP)
+    global tim2
+    tim2 = pyb.Timer(2, freq=20000)
+    global ch2
+    ch2 = tim2.channel(1, pyb.Timer.PWM_INVERTED, pin=pinA0)
+
+def led_brightness(brightness):
+    """"!
+    Changes LED brightness.
+    @param brightness Uses a value of 0 - 100 to set brightness.
+    """
+    ch2.pulse_width_percent(brightness)
+    
+def main():
+    """!
+    Main function that loops the changing of brightness over 10 seconds.
+    """
+    led_setup()
+    while True:
+        tot_time = 10
+        rate = tot_time/20
+        for i in range(10):
+            led_brightness(i*10)
+            time.sleep(rate)
+        for i in range(10):
+            led_brightness(100-i*10)
+            time.sleep(rate)
+            
+if __name__ == "__main__":
+    main()
+    
+
--- src/main.py
+++ src/main.py
@@ -1,46 +1,16 @@
-"""! @file led.py
-Slowly increases and then decreases LED brightness over 10 seconds.
-Requires LED with anode connected to 3v3 pin on Arduino and cathode connected to pin A0 on Arduino.
-Place a resistor in circuit as well.
+""" @file main.py
+@author Nathaniel Davis
+@author Sebastian Bessoudo
+@date 02-05-2024
+This is a test for the motor driver class.
+Imports the MotorDriver class, then tests it.
+A value above +100 or below -100 will simply saturate the PWM.
 """
 
-import pyb
-import time
+from motor_driver import MotorDriver
 
-def led_setup():
-    """"!
-    Sets up LED configuration by definind output pin and timer to be used for PWM.
-    """
-    global A0
-    pinA0 = pyb.Pin(pyb.Pin.board.PA0, pyb.Pin.OUT_PP)
-    global tim2
-    tim2 = pyb.Timer(2, freq=20000)
-    global ch2
-    ch2 = tim2.channel(1, pyb.Timer.PWM_INVERTED, pin=pinA0)
-
-def led_brightness(brightness):
-    """"!
-    Changes LED brightness.
-    @param brightness Uses a value of 0 - 100 to set brightness.
-    """
-    ch2.pulse_width_percent(brightness)
-    
-def main():
-    """!
-    Main function that loops the changing of brightness over 10 seconds.
-    """
-    led_setup()
-    while True:
-        tot_time = 10
-        rate = tot_time/20
-        for i in range(10):
-            led_brightness(i*10)
-            time.sleep(rate)
-        for i in range(10):
-            led_brightness(100-i*10)
-            time.sleep(rate)
-            
 if __name__ == "__main__":
-    main()
-    
-
+    moe = MotorDriver(pyb.Pin.board.PA10, pyb.Pin.board.PB4, 1,  pyb.Pin.board.PB5, 2, 3)
+    while True:
+        user = input("Please enter a motor speed from -100 to +100: ")
+        moe.set_duty_cycle(float(user))
